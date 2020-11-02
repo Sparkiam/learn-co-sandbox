c443e0b8e66cea317d49bd512aad4dd1fff8358e
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-11-02 18:55:40 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,20 @@
+#puts "Joy to the world"
+#puts "All thr boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+#puts "Joy to you and me"
+
+#name = "Luca"
+
+#puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the stoe"
+
+
+new_hash = { :created => Time.now, :message => "Joy to the world" } new_hash 
+
+
+run_code_inside = true 
+puts "Code before if...end"
+  if
+run_code_inside
+    puts "code inside"
+    end 
+    puts "Code after if...end"
